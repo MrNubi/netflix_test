df5136fe2c158d7247ebcca8685261ecdc18ad1b
jahun <99630193+MrNubi@users.noreply.github.com>
2022-11-30 23:00:58 +0900

    pages폴더 작성 commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import requests from '../src/api/request';
-import Banner from './components/Banner';
 import Nav from './components/Nav';
-import Row from './components/Row';
 import './App.css';
 import Footer from './components/Footer';
-import Clock2 from './components/clock/Clock2';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav></Nav>
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="app">
-      <Nav />
-
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchURL={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      ></Row>
-      <Row
-        title="Trending Now"
-        id="TN"
-        fetchURL={requests.fetchTrending}
-        isLargeRow={false}
-      ></Row>
-      <Row
-        title="TOP Rated"
-        id="TR"
-        fetchURL={requests.fetchTopRated}
-        isLargeRow={false}
-      ></Row>
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchURL={requests.fetchActionMovies}
-        isLargeRow={false}
-      ></Row>
-
-      <Row
-        title="Comedy"
-        id="CM"
-        fetchURL={requests.fetchComedyMovies}
-        isLargeRow={false}
-      ></Row>
-
-      <Footer></Footer>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+      <Route index element={<MainPage />} />
+      <Route path=":movieId" element={<DetailPage />} />
+      <Route path="search" element={<SearchPage />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Banner from '../../components/Banner';
+import Row from '../../components/Row';
+
+export default function MainPage() {
+  return (
+    <div className="app">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      ></Row>
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchURL={requests.fetchTrending}
+        isLargeRow={false}
+      ></Row>
+      <Row
+        title="TOP Rated"
+        id="TR"
+        fetchURL={requests.fetchTopRated}
+        isLargeRow={false}
+      ></Row>
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchURL={requests.fetchActionMovies}
+        isLargeRow={false}
+      ></Row>
+
+      <Row
+        title="Comedy"
+        id="CM"
+        fetchURL={requests.fetchComedyMovies}
+        isLargeRow={false}
+      ></Row>
+    </div>
+  );
+}
